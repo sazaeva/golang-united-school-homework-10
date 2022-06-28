e8568b894b68c7f3e651d6dcfdd24055d3e980b8
sazaeva <janylsazaeva@gmail.com>
2022-06-28 13:31:50 +0300

    Web requests handling

--- cmd/muxing/muxing.go
+++ cmd/muxing/muxing.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -20,11 +21,53 @@ main function reads host/port from env just for an example, flavor it following 
 // Start /** Starts the web server listener on given host and port.
 func Start(host string, port int) {
 	router := mux.NewRouter()
+	router.HandleFunc("/name/{PARAM}", handleName).Methods(http.MethodGet)
+	router.HandleFunc("/bad/", handleBad).Methods(http.MethodGet)
+	router.HandleFunc("/data", handleData).Methods(http.MethodPost)
+	router.HandleFunc("/header/", handleHeader).Methods(http.MethodPost)
 
-	log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
-	if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	http.NewServeMux()
+	log.Fatalln(http.ListenAndServe(":8081", router))
+	//log.Println(fmt.Printf("Starting API server on %s:%d\n", host, port))
+	//if err := http.ListenAndServe(fmt.Sprintf("%s:%d", host, port), router); err != nil {
+	//	log.Fatal(err)
+	//}
+}
+
+func handleName(w http.ResponseWriter, r *http.Request) {
+	name := "mister X"
+	if p, ok := mux.Vars(r)["PARAM"]; ok {
+		name = p
+	}
+	fmt.Fprintf(w, "Hello, %s!", name)
+}
+
+func handleBad(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusBadGateway)
+	w.Write([]byte("Oh, smth bad happened on server side. Please contact the developers"))
+}
+
+func handleData(w http.ResponseWriter, r *http.Request) {
+	str, err := io.ReadAll(r.Body)
+	if err == nil {
+		w.Write([]byte("I got massage:\n" + string(str)))
+	}
+
+}
+
+func handleHeader(w http.ResponseWriter, r *http.Request) {
+	x := r.Header.Get("a")
+	y := r.Header.Get("b")
+
+	a, err := strconv.Atoi(x)
+	if err != nil {
 		log.Fatal(err)
 	}
+	b, err := strconv.Atoi(y)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Header().Add("a+b", strconv.Itoa(a+b))
 }
 
 //main /** starts program, gets HOST:PORT param and calls Start func.
